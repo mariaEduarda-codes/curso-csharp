ad50c1c34166031925b61a417eb58667e4aa0907
Maria Eduarda <78294169+mariaEduarda-codes@users.noreply.github.com>
2021-08-01 03:55:59 -0300

    Create Retangulo.cs

---
+++ OO/Exercício1/Retangulo.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Exercicio1 {
+    class Retangulo {
+        public double Largura;
+        public double Altura;
+
+        public double Area() {
+            return Largura * Altura;
+        }
+
+        public double Perimetro() {
+            return (Largura * 2) + (Altura * 2);
+        }
+
+        public double Diagonal() {
+            return Math.Sqrt(Math.Pow(Largura, 2) + Math.Pow(Altura, 2));
+        }
+    }
+}
